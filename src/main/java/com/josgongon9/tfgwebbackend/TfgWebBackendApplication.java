7eda791de31c6662f52d6916ad6fc1fa947b9761
Jose Miguel <josemiguelgonzalez14@hotmail.com>
2022-02-19 12:03:41 +0100

    Swagger implemetation

--- src/main/java/com/josgongon9/tfgwebbackend/TfgWebBackendApplication.java
+++ src/main/java/com/josgongon9/tfgwebbackend/TfgWebBackendApplication.java
@@ -2,6 +2,10 @@ package com.josgongon9.tfgwebbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class TfgWebBackendApplication  {
@@ -10,6 +14,13 @@ public class TfgWebBackendApplication  {
 		SpringApplication.run(TfgWebBackendApplication.class, args);
 	}
 
-
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource(){
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setFallbackToSystemLocale(Boolean.FALSE);
+		messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+		messageSource.setBasename("classpath:locale/messages");
+		return messageSource;
+	}
 
 }
