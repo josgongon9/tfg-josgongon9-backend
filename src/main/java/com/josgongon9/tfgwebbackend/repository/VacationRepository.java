32c42f408b5c7eb9c4be47effcd38822b5e465d4
Jose Miguel <josemiguelgonzalez14@hotmail.com>
2022-05-20 14:43:58 +0200

    SearchText

--- src/main/java/com/josgongon9/tfgwebbackend/repository/VacationRepository.java
+++ src/main/java/com/josgongon9/tfgwebbackend/repository/VacationRepository.java
@@ -2,6 +2,7 @@ package com.josgongon9.tfgwebbackend.repository;
 
 import java.util.List;
 
+import com.josgongon9.tfgwebbackend.model.Organization;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.josgongon9.tfgwebbackend.model.Vacation;
@@ -12,4 +13,6 @@ public interface VacationRepository extends MongoRepository<Vacation, String> {
     List<Vacation> findByPublished(boolean published);
 
     List<Vacation> findByTitleContaining(String title);
+
+    List<Vacation> findAllByTitleLike(String title);
 }
